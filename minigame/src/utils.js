95c7bf182dd9169eb152fc8abb3db74aa634aa15
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-08-29 22:32:17 +0100

    css completed

---
+++ minigame/src/utils.js
@@ -0,0 +1,4 @@
+export function displayDialogue(text, onDisplayEnd) {
+  const dialogueUI = document.getElementById("textbox-container");
+  //   const dialogue = document.getElementById("dialogue");
+}
