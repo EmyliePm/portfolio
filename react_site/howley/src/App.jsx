e8c7f98f1c9735709ceec5d26d6240d97b0207d7
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-09-10 13:26:31 +0100

    adding title component and css styling to use across site and staging it into App.jsx

--- react_site/howley/src/App.jsx
+++ react_site/howley/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import AboutUs from "./Components/AboutUs/AboutUs";
 import Hero from "./Components/Hero/hero";
+import Title from "./Components/Title/title";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <div className="container">
+        <Title subTitle="About Us" title="Get to know us" />
         <AboutUs />
       </div>
     </div>
