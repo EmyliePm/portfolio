25dc78f56c75d8bb3a0338ea01ccd768ecf54b46
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-09-10 13:03:57 +0100

    addec hover images on the About Us section (aboutUs.html, aboutUs.css and icon images in assets folder.

--- react_site/howley/src/Components/AboutUs/AboutUs.jsx
+++ react_site/howley/src/Components/AboutUs/AboutUs.jsx
@@ -3,6 +3,9 @@ import "./AboutUs.css";
 import worker1 from "../../assets/clay-elliot-PF7mP2xCeQw-unsplash.jpg";
 import worker2 from "../../assets/brooke-cagle-JBwcenOuRCg-unsplash.jpg";
 import worker3 from "../../assets/bruce-mars-8YG31Xn4dSw-unsplash.jpg";
+import workerIcon1 from "../../assets/economy.png";
+import workerIcon2 from "../../assets/piggy-bank.png";
+import workerIcon3 from "../../assets/presentation.png";
 
 const AboutUs = () => {
   return (
@@ -10,18 +13,21 @@ const AboutUs = () => {
       <div className="worker">
         <img src={worker1} alt="worker"></img>{" "}
         <div className="caption">
+          <img src={workerIcon1} alt="icon"></img>
           <p>Paul Bowden (FCCA, ACA, Bsc)</p>
         </div>
       </div>
       <div className="worker">
         <img src={worker2} alt="worker"></img>{" "}
         <div className="caption">
+          <img src={workerIcon2} alt="icon"></img>
           <p>Paulita Berry (FCCA, MAAT)</p>
         </div>
       </div>
       <div className="worker">
         <img src={worker3} alt="worker"></img>{" "}
         <div className="caption">
+          <img src={workerIcon3} alt="icon"></img>
           <p>Tim Ware (FCCA, Prince2, ILM)</p>
         </div>
       </div>
