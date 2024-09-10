d55f6badd8e8af57a50f806a0eebb271f1821872
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-09-10 17:29:30 +0100

    adding contact form area jsx and css

--- react_site/howley/src/App.jsx
+++ react_site/howley/src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./Components/Hero/hero";
 import Title from "./Components/Title/title";
 import Services from "./Components/Services/Services";
 import Testimonials from "./Components/Testimonials/Testimonials";
+import Contact from "./Components/Contact/Contact";
 
 const App = () => {
   return (
@@ -17,6 +18,8 @@ const App = () => {
         <Services />
         <Title subTitle="Testimonials" title="What Clients Say" />
         <Testimonials />
+        <Title subTitle="Contact Us" title="Book a free consultation now!" />
+        <Contact />
       </div>
     </div>
   );
---
+++ react_site/howley/src/Components/Contact/Contact.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "./Contact.css";
+import msg_icon from "../../assets/messenger.png";
+import mail_icon from "../../assets/mail-icon.png";
+import phone_icon from "../../assets/phone-icon.png";
+import location_icon from "../../assets/location-icon.png";
+import white_arrow from "../../assets/white-arrow.png";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="contact-col">
+        <h3>
+          Send us a message <img src={msg_icon} />
+        </h3>
+        <p>
+          Feel free to reach out through our contact form or find our contact
+          information below.
+        </p>
+        <ul>
+          <li>
+            {" "}
+            <img src={mail_icon} />
+            admin@paulhowley.co.uk
+          </li>
+          <li>
+            <img src={phone_icon} />
+            01226 205154
+          </li>
+          <li>
+            <img src={location_icon} />
+            Paul Howley & Co <br />
+            42 Pitt Street <br /> Barnsley <br /> S70 1BB
+          </li>
+        </ul>
+      </div>
+      <div className="contact-col">
+        <form>
+          <label>Your Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          ></input>
+          <label>Phone Number</label>
+          <input
+            type="tell"
+            name="phone"
+            placeholder="Enter your phone number"
+            required
+          />
+          <label>Write your message here</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            Submit <img src={white_arrow} />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
